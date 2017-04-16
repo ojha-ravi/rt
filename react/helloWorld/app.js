57ecb2c2f439be112eb6e3f20081c3b0ce839fb9
rbojha <rbojha008@gmail.com>
2017-04-16 21:34:44 +0530

    Adding Some more examples

--- react/helloWorld/app.js
+++ react/helloWorld/app.js
@@ -1,8 +1,119 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+// Very Basic
 export class App extends React.Component {
   render() {
     return <div>Hello World</div>;
   }
 }
+
+// Just trying follow all the lifeCycleMethods
+// export class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//       this.state = {
+//         data: 0
+//       }
+//       this.setNewNumber = this.setNewNumber.bind(this)
+//   };
+//   setNewNumber() {
+//     this.setState({data: this.state.data + 1})
+//   }
+//   render() {
+//     return <div>
+//       <Content myNumber = {this.state.data}></Content>
+//       <button onClick = {this.setNewNumber} className="btn btn-secondary">Increment</button>
+//     </div>;
+//   }
+// }
+
+
+// class Content extends React.Component {
+//   componentWillMount() {
+//     console.log("I am in componentWillMount");
+//   }
+//   componentDidMount() {
+//     console.log("I am in componentDidMount");
+//   }
+//   componentWillReceiveProps(nextProps) {
+//     console.log("I am in componentWillReceiveProps");
+//   }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log("I am in shouldComponentUpdate");
+//     return true;
+//   }
+//   componentWillUpdate(nextProps, nextState) {
+//     console.log("I am in componentWillUpdate");
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     console.log("I am in componentDidUpdate");
+//   }
+//   componentWillUnmount() {
+//     console.log("I am in componentWillUnmount");
+//   }
+//   render() {
+//     return <div>
+//       <h3>Hello World</h3>
+//       <div>Random Number: {this.props.myNumber}</div>
+//     </div>;
+//   }
+// }
+
+// // Trying to simulate SetState
+// export class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: 'Initial data...'
+//     }
+//     this.updateState = this.updateState.bind(this);
+//   };
+
+  // updateState(e) {
+  //   this.setState({data: e.target.value});
+  // }
+
+  // render() {
+  //   return <div>
+  //     <input
+  //       type="text"
+  //       value={this.state.data} 
+  //       onChange = {this.updateState}></input>
+  //     <h4>{this.state.data}</h4>
+  //   </div>;
+  // }
+// }
+
+
+// A Bit complex Example
+// class App extends React.Component {
+//   constructor(props) {
+//   super(props);
+//     this.state = {
+//       data: 'Initial data...'
+//     }
+//     this.updateState = this.updateState.bind(this);
+//   };
+
+//   updateState(e) {
+//     this.setState({data: e.target.value});
+//   }
+
+//   render() {
+//     return <div>
+//       <Content myDataProp = {this.state.data} 
+//       updateStateProp = {this.updateState}></Content>
+//     </div>;
+//   }
+// }
+
+// class Content extends React.Component {
+//   render() {
+//     return <div>
+//       <input type = "text" value = {this.props.myDataProp} 
+//       onChange = {this.props.updateStateProp} />
+//       <h3>{this.props.myDataProp}</h3>
+//     </div>;
+//   }
+// }
--- react/helloWorld/main.js
+++ react/helloWorld/main.js
@@ -3,3 +3,9 @@ import * as ReactDOM from 'react-dom';
 import { App } from "./app";
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
+
+// Trying to simulate Unmount
+// setTimeout(() => {
+//    ReactDOM.unmountComponentAtNode(document.querySelector('.container'));
+// }, 10000);
+
