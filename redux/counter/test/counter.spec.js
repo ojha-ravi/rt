23af25b3912d72fbee0c69093c18b5df12a5f50d
rbojha <rbojha008@gmail.com>
2017-04-18 15:33:41 +0530

    Some more modification

--- redux/counter/test/counter.spec.js
+++ redux/counter/test/counter.spec.js
@@ -1,6 +1,41 @@
 const expect = require("expect");
 const deepFreezee = require("deep-freeze");
 
+// Testing counter
+const counter = (state = 0, action) => {
+	console.log(action);
+	switch(action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+expect(
+	counter(0, {type: "INCREMENT"})
+).toEqual(1);
+
+expect(
+	counter(1, {type: "INCREMENT"})
+).toEqual(2);
+
+expect(
+	counter(2, {type: "DECREMENT"})
+).toEqual(1);
+
+expect(
+	counter(1, {type: "DECREMENT"})
+).toEqual(0);
+
+expect(
+	counter(1, {type: "SOMETHING"})
+).toEqual(1);
+
+console.log("Counter Reducer Test Passes!!");
+
 const addCounter = (list) => {
 	return [...list, 0];
 	// return list;
