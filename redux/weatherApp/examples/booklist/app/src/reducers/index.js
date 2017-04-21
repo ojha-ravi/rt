4a7e0bc6accf471f16764e32ecfa279d5ae9ff77
rbojha <rbojha008@gmail.com>
2017-04-21 13:13:19 +0530

    Deleting Some files

--- redux/weatherApp/examples/booklist/app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { BooksReducer } from "./reducer_books" 
-import { ActiveBookReducer } from "./reducer_active_book"
-
-export const rootReducer = combineReducers({
-	books: BooksReducer,
-	activeBook: ActiveBookReducer
-});
