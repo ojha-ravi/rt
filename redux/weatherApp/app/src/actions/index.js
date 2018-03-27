d4fa7aacd0158f2705a1bff8fb0314a6c079b8c8
Ravi Ojha <rbojha008@gmail.com>
2018-03-28 01:06:06 +0530

    Some more changes

--- redux/weatherApp/app/src/actions/index.js
+++ redux/weatherApp/app/src/actions/index.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_KEY = "f270f91bc1fc38715a4a0675dc174309";
+const API_KEY = 'f270f91bc1fc38715a4a0675dc174309';
 
-export const FETCH_WEATHER = "FETCH_WEATHER";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
-	const url = `${ROOT_URL}&q=${city},us`;
-	const request = axios.get(url).then();
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url).then();
 
-	return {
-		type: FETCH_WEATHER,
-		payload: request
-	}
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
 }
--- redux/weatherApp/app/src/conatiners/weather_list.js
+++ redux/weatherApp/app/src/conatiners/weather_list.js
@@ -1,43 +1,58 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import { Chart } from "../components/chart"
-import { GoogleMap } from "../components/google_map";
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Chart } from '../components/chart';
+import { GoogleMap } from '../components/google_map';
 
 class WeatherList extends React.Component {
   renderWeather(cd) {
-    const cityName = cd.city.name;
-    const temps = cd.list.map(w => w.main.temp);
-    const pressures = cd.list.map(w => w.main.pressure);
-    const humidities = cd.list.map(w => w.main.humidity);
-    const {lon, lat} = cd.city.coord;
+    if (!cd) {
+      return null;
+    }
+    const cityName = cd.name;
+    const temps = [cd.main.temp];
+    const pressures = [cd.main.pressure];
+    const humidities = [cd.main.humidity];
+    const { lon, lat } = cd.coord;
 
     console.log(temps, pressures, humidities);
 
-    return <tr key={cityName}>
-      <td><GoogleMap lat={lat} lon={lon}></GoogleMap></td>
-      <td><Chart data={temps} color="red" units={"K"}></Chart></td>
-      <td><Chart data={pressures} color="green" units={"hPa"}></Chart></td>
-      <td><Chart data={humidities} color="blue" units={"%"}></Chart></td>
-    </tr>;
+    return (
+      <tr key={cityName}>
+        <td>
+          <GoogleMap lat={lat} lon={lon} />
+        </td>
+        <td>
+          <Chart data={temps} color="red" units={'K'} />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units={'hPa'} />
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" units={'%'} />
+        </td>
+      </tr>
+    );
   }
 
   render() {
-    return <table className="table table-hover">
-      <thead>
-        <tr>
-          <th>City</th>
-          <th>Temperature (K)</th>
-          <th>Pressure (hPa)</th>
-          <th>Humidity (%)</th>
-        </tr>
-      </thead>
-      <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
-    </table>
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+      </table>
+    );
   }
 }
 
-function mapStateToProps({weather}) {
+function mapStateToProps({ weather }) {
   return { weather };
 }
 
-export const WeatherListConnect = connect(mapStateToProps)(WeatherList)
+export const WeatherListConnect = connect(mapStateToProps)(WeatherList);
